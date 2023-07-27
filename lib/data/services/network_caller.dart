15582e4ba773583b7c9549c86537e184b000e7ec
fakhruddin07 <fakhruddinnoman95@gmail.com>
2023-07-27 13:59:50 +0600

    sign up validation have been done

---
+++ lib/data/services/network_caller.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:http/http.dart';
+import '../models/network_response.dart';
+
+class NetworkCaller {
+  Future<NetworkResponse> getRequest(String url) async {
+    try {
+      Response response = await get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        return NetworkResponse(
+            true, response.statusCode, jsonDecode(response.body));
+      } else {
+        return NetworkResponse(false, response.statusCode, null);
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return const NetworkResponse(false, -1, null);
+  }
+
+  Future<NetworkResponse> postRequest(
+      String url, Map<String, dynamic> body) async {
+    try {
+      Response response = await post(
+        Uri.parse(url),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(body),
+      );
+      if (response.statusCode == 200) {
+        return NetworkResponse(
+            true, response.statusCode, jsonDecode(response.body));
+      } else {
+        return NetworkResponse(false, response.statusCode, null);
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return const NetworkResponse(false, -1, null);
+  }
+}
--- lib/ui/screens/auth/signup_screen.dart
+++ lib/ui/screens/auth/signup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:task_manager_project_using_rest_api/ui/screens/auth/email_verification_screen.dart';
+import 'package:task_manager_project_using_rest_api/data/services/network_caller.dart';
+import 'package:task_manager_project_using_rest_api/data/utility/urls.dart';
 import '../../../widgets/screen_background.dart';
 
 class SignupScreen extends StatefulWidget {
@@ -10,83 +11,187 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController _emailTEController = TextEditingController();
+  final TextEditingController _firstNameTEController = TextEditingController();
+  final TextEditingController _lastNameTEController = TextEditingController();
+  final TextEditingController _mobileTEController = TextEditingController();
+  final TextEditingController _passwordTEController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _isSignUpInProgress = false;
+
+  Future<void> userSignUp() async {
+    _isSignUpInProgress = true;
+    if(mounted){
+      setState(() {});
+    }
+
+    final response = await NetworkCaller().postRequest(
+      Urls.registration,
+      <String, dynamic>{
+        "email": _emailTEController.text.trim(),
+        "firstName": _firstNameTEController.text.trim(),
+        "lastName": _lastNameTEController.text.trim(),
+        "mobile": _mobileTEController.text.trim(),
+        "password": _passwordTEController.text,
+        "photo": "",
+      },
+    );
+
+    if (response.isSuccess) {
+      _emailTEController.clear();
+      _firstNameTEController.clear();
+      _lastNameTEController.clear();
+      _mobileTEController.clear();
+      _passwordTEController.clear();
+
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Registration Success"),
+          ),
+        );
+      }
+    }else{
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Registration fa"),
+          ),
+        );
+      }
+    }
+    _isSignUpInProgress = false;
+    if(mounted){
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ScreenBackground(
         child: SingleChildScrollView(
           padding: const EdgeInsets.all(25),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 65),
-              Text(
-                "Join With Us",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              const SizedBox(height: 16),
-              const TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  hintText: "Email",
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 65),
+                Text(
+                  "Join With Us",
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
-              ),
-              const SizedBox(height: 12),
-              const TextField(
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  hintText: "First Name",
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _emailTEController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    hintText: "Email",
+                  ),
+                  validator: (String? value) {
+                    if (value?.isEmpty ?? true) {
+                      return "Enter your email";
+                    }
+                    return null;
+                  },
                 ),
-              ),
-              const SizedBox(height: 12),
-              const TextField(
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  hintText: "Last Name",
+                const SizedBox(height: 12),
+                TextFormField(
+                  controller: _firstNameTEController,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                    hintText: "First Name",
+                  ),
+                  validator: (String? value) {
+                    if (value?.isEmpty ?? true && value!.length <= 4) {
+                      return "Enter your first name";
+                    }
+                    return null;
+                  },
                 ),
-              ),
-              const SizedBox(height: 12),
-              const TextField(
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                  hintText: "Mobile",
+                const SizedBox(height: 12),
+                TextFormField(
+                  controller: _lastNameTEController,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                    hintText: "Last Name",
+                  ),
+                  validator: (String? value) {
+                    if (value?.isEmpty ?? true && value!.length <= 4) {
+                      return "Enter your last name";
+                    }
+                    return null;
+                  },
                 ),
-              ),
-              const SizedBox(height: 12),
-              const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: "Password",
+                const SizedBox(height: 12),
+                TextFormField(
+                  controller: _mobileTEController,
+                  keyboardType: TextInputType.phone,
+                  decoration: const InputDecoration(
+                    hintText: "Mobile",
+                  ),
+                  validator: (String? value) {
+                    if ((value?.isEmpty ?? true) || value!.length < 11) {
+                      return "Enter your valid mobile number";
+                    }
+                    return null;
+                  },
                 ),
-              ),
-              const SizedBox(height: 16),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Icon(Icons.arrow_forward_ios_rounded),
+                const SizedBox(height: 12),
+                TextFormField(
+                  controller: _passwordTEController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: "Password",
+                  ),
+                  validator: (String? value) {
+                    if ((value?.isEmpty ?? true) || value!.length < 8) {
+                      return "Enter your password";
+                    }
+                    return null;
+                  },
                 ),
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "Have an account?",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 0.5,
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: Visibility(
+                    visible: _isSignUpInProgress == false,
+                    replacement: const Center(child: CircularProgressIndicator()),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (!_formKey.currentState!.validate()) {
+                          return;
+                        } else {
+                          userSignUp();
+                        }
+                      },
+                      child: const Icon(Icons.arrow_forward_ios_rounded),
                     ),
                   ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text("Sign in"),
-                  ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Have an account?",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 0.5,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text("Sign in"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
